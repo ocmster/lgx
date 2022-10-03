9eee9a088dd970afa9f1302e43da0dbf499266ab
ocmster <16679722@qq.com>
2022-10-03 20:42:12 +0800

    Update main.py

--- main.py
+++ main.py
@@ -8,9 +8,8 @@ from github import Github
 from feedgen.feed import FeedGenerator
 from lxml.etree import CDATA
 
-MD_HEAD = """## Gitblog
-My personal blog using issues and GitHub Actions    
-[RSS Feed](https://raw.githubusercontent.com/{repo_name}/main/feed.xml)   
+MD_HEAD = """**<p align="center">[ocmster's Blog]</p>**
+**<p align="center">用于记录一些个人认知及学习路上的思考~</p>**
 """
 
 BACKUP_DIR = "BACKUP"
